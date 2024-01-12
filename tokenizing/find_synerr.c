f0ef73fd91c022f246546917998566318efcb894
Minkyeong Lee <minkylee@c5r4s7.42seoul.kr>
2024-01-12 14:34:15 +0900

    fix norm

--- tokenizing/find_synerr.c
+++ tokenizing/find_synerr.c
@@ -6,16 +6,15 @@
 /*   By: minkylee <minkylee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 17:46:49 by minkylee          #+#    #+#             */
-/*   Updated: 2024/01/10 15:01:15 by minkylee         ###   ########.fr       */
+/*   Updated: 2024/01/11 20:42:58 by minkylee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../microshell.h"
 
-int pipe_at_the_end(char *buf)
+int	pipe_at_the_end(char *buf)
 {
-	int i;
-
+	int	i;
 
 	i = ft_strlen(buf) - 1;
 	while (i >= 0 && buf[i] == 'S')
@@ -26,41 +25,44 @@ int pipe_at_the_end(char *buf)
 			printf("minishell: syntax error near unexpected token `|'\n");
 		else if (buf[i - 1] == 'p')
 			printf("minishell: syntax error near unexpected token `||'\n");
-		return 0;
+		return (0);
 	}
 	else if (i == 0 && buf[i] == 'p')
 	{
 		printf("minishell: syntax error near unexpected token `|'\n");
-		return 0;
+		return (0);
 	}
-	return 1;
+	return (1);
 }
 
-int only_redirec(char *buf)
+int	only_redirec(char *buf)
 {
-    int i = 0;
-    int redirection_found = 0; // 리다이렉션 기호가 발견되었는지 추적합니다.
+	int	i;
+	int	redirection_found;
 
-    while (buf[i])
-    {
-        if (buf[i] == 'r')
-            redirection_found = 1;
-        else if (buf[i] != 'S' && buf[i] != 'r')
-            return 0;
-        i++;
-    }
-    if (redirection_found)
-        return 1;
-    return 0;
+	i = 0;
+	redirection_found = 0;
+	while (buf[i])
+	{
+		if (buf[i] == 'r')
+			redirection_found = 1;
+		else if (buf[i] != 'S' && buf[i] != 'r')
+			return (0);
+		i++;
+	}
+	if (redirection_found)
+	{
+		printf("minishell: syntax error near unexpected token `newline'\n");
+		return (1);
+	}
+	return (0);
 }
 
-
-int find_syntax_err(char *buf)
+int	find_syntax_err(char *buf)
 {
-	int status;
+	int	status;
 
-    if (pipe_at_the_end(buf) && !only_redirec(buf))
-		return 1;
-    return 0;
-    
+	if (pipe_at_the_end(buf) && !only_redirec(buf))
+		return (1);
+	return (0);
 }
